6b491d9c0490a8b41e1a5d6895f1bbe9e19ad861
Ramiro Guzman <rami.guzmi@gmail.com>
2022-12-27 13:53:15 -0300

    Rotacion de la torreta

--- Final Desarrollo II/Assets/Scrips/TowerTurns.cs
+++ Final Desarrollo II/Assets/Scrips/TowerTurns.cs
@@ -4,20 +4,16 @@ using UnityEngine;
 
 public class TowerTurns : MonoBehaviour
 {
+    [SerializeField] float duration;
+    Coroutine turnTower;
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
-            Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
-
-            Vector3 direction = mouseOnScreen - positionOnScreen;
-
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
-            transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));        
+            StartCoroutine(TurnTower());
         }
-        
+
 
     }
 
@@ -29,7 +25,30 @@ public class TowerTurns : MonoBehaviour
 
     IEnumerator TurnTower()
     {
-        yield return null;
+        float timer = 0;
+
+        Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        Vector3 direction = mouseOnScreen - positionOnScreen;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90.0f;
+
+        Quaternion initialRotation = transform.rotation;
+        Quaternion newRotation = Quaternion.Euler(new Vector3(0, -angle, 0));
+
+        while (timer <= duration)
+        {
+            float interpolationValue = timer / duration;
+
+            transform.rotation = Quaternion.Lerp(initialRotation, newRotation, interpolationValue);
+
+
+            timer += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.rotation = newRotation;
         Shoot();
     }
 }
