65ea8be8392e54a3fbf6435f5da458c8258e89eb
Ramiro Guzman <rami.guzmi@gmail.com>
2023-01-23 13:30:56 -0300

    Funcionalidades del menu completas

---
+++ Final Desarrollo II/Assets/Scrips/UI/MenuOptions.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuOptions : MonoBehaviour
+{
+
+    public void GoToGameplay()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }   
+    
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+#else   
+
+        Application.Quit();
+#endif
+
+    }
+
+
+}
