ee80eabaca04d4ec681b45031fa6ac90e06bed09
Ramiro Guzman <rami.guzmi@gmail.com>
2023-01-27 16:38:06 -0300

    Lectura de los creditos por medio del textAsset

--- Final Desarrollo II/Assets/Scrips/Menu/CreditsParser.cs
+++ Final Desarrollo II/Assets/Scrips/Menu/CreditsParser.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CreditsParser : MonoBehaviour
 {
 
+   [SerializeField] TextAsset creditsTextAsset;
+   [SerializeField] TextMeshProUGUI cretitsText;
+
+    private void Start()
+    {
+        cretitsText.text = creditsTextAsset.text;
 
 
+    }
 
 
 
